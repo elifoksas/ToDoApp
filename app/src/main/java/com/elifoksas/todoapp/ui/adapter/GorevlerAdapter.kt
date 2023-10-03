6a7eec2e745cc77a567872e03588d427bf28eddc
elifoksas <62424342+elifoksas@users.noreply.github.com>
2023-10-03 21:50:21 +0300

    added Adapter

---
+++ app/src/main/java/com/elifoksas/todoapp/ui/adapter/GorevlerAdapter.kt
@@ -0,0 +1,40 @@
+package com.elifoksas.todoapp.ui.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import com.elifoksas.todoapp.R
+import com.elifoksas.todoapp.data.entity.Gorevler
+import com.elifoksas.todoapp.databinding.ItemTasarimBinding
+import com.elifoksas.todoapp.utils.gecis
+
+class GorevlerAdapter (private val mContext:Context,
+                       private val gorevlerListesi:List<Gorevler>) : RecyclerView.Adapter<GorevlerAdapter.TasarimTutucu>(){
+
+    inner class TasarimTutucu(val binding: ItemTasarimBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TasarimTutucu {
+        val binding = ItemTasarimBinding.inflate(LayoutInflater.from(mContext),parent,false)
+        return TasarimTutucu(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return gorevlerListesi.size
+    }
+    override fun onBindViewHolder(holder: TasarimTutucu, position: Int) {
+        val binding = holder.binding
+        val gorev = gorevlerListesi[position]
+
+        binding.textViewGorev.text = gorev.gorev_ad
+
+        binding.cardViewGorev.setOnClickListener {
+            Navigation.gecis(it, R.id.detayFragmentGecis)
+        }
+
+
+    }
+}
