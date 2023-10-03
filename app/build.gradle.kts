42695e01bcc36132a5a63752d356b114b95fbf16
elifoksas <62424342+elifoksas@users.noreply.github.com>
2023-10-03 14:53:06 +0300

    implemented navigation component

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,4 +44,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.2")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.2")
 }
